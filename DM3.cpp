2319870b1b71c6709ea1367b40fadef0452b0b37
Zevir <Zevir@WIN-HKVT92CM4GO>
2025-01-28 20:00:46 +0900

    Добавьте файлы проекта.

---
+++ DM3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    // Ввод значений
+    double a, b, c;
+    cout << "Введите a, b, c: ";
+    cin >> a >> b >> c;
+
+    // Проверка неравенств
+    bool prov1 = (a < b);
+    bool prov2 = (b < c);
+
+    // Вывод результата
+    if (prov1 && prov2) {
+        cout << "Неравенства выполняются: a < b < c" << endl;
+    }
+    else {
+        cout << "Неравенства не выполняются" << endl;
+    }
+
+    return 0;
+}
